d499e36a746cc7137f390ebccc7d9546ccdd33ad
Kaustubh Ladiya <k4ustu3h@gmail.com>
2020-03-15 22:25:19 +0530

    Updates and Fixes
    Updated
      - acorn
      - got
      - jsdom
      - snyk
      - stylelint
    Fixed
      - Moved script to populate.js so that username and bio variable can work.

--- populate.js
+++ populate.js
@@ -118,16 +118,12 @@ module.exports.updateHTML = (username, opts) => {
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:title" content="${user.login}" />
           <meta name="twitter:description" content="${user.bio}" />`;
+          //Socials
           document.getElementById(
             "username"
           ).innerHTML = `<span id="text" style="display:${
             user.name == null || !user.name ? "none" : "block"
           };"></span><div class='console-underscore' id='console'>&#95;</div>`;
-          document.getElementById("userbio").innerHTML = convertToEmoji(
-            user.bio
-          );
-          document.getElementById("userbio").style.display =
-            user.bio == null || !user.bio ? "none" : "block";
           document.getElementById("about").innerHTML = `
                 <span style="display:${
                   user.company == null || !user.company ? "none" : "block"
@@ -194,6 +190,86 @@ module.exports.updateHTML = (username, opts) => {
                 };"><a href="https://forum.xda-developers.com/member.php?u=${xda}" target="_blank" class="socials"><span class="iconify" data-icon="mdi-xda"></span></a></span>
                 </div>
                 `;
+          //Script
+          document.getElementById("script").innerHTML = `<script>
+            const magicProjectsGrid = new MagicGrid({
+              container: "#work_section",
+              animate: false,
+              gutter: 30, // default gutter size
+              static: true,
+              useMin: false,
+              maxColumns: 2,
+              useTransform: true
+            });
+
+            const magicForksGrid = new MagicGrid({
+              container: "#forks_section",
+              animate: false,
+              gutter: 30, // default gutter size
+              static: true,
+              useMin: false,
+              maxColumns: 2,
+              useTransform: true
+            });
+
+            $("document").ready(() => {
+              magicProjectsGrid.listen();
+              magicForksGrid.listen();
+            });
+
+            // function([string1, string2],target id,[color1,color2])
+            consoleText(["${user.name}", "${user.bio}"], "text", [
+              "white",
+              "white"
+            ]);
+
+            function consoleText(words, id, colors) {
+              if (colors === undefined) colors = ["#fff"];
+              var visible = true;
+              var con = document.getElementById("console");
+              var letterCount = 1;
+              var x = 1;
+              var waiting = false;
+              var target = document.getElementById(id);
+              target.setAttribute("style", "color:" + colors[0]);
+              window.setInterval(function() {
+                if (letterCount === 0 && waiting === false) {
+                  waiting = true;
+                  target.innerHTML = words[0].substring(0, letterCount);
+                  window.setTimeout(function() {
+                    var usedColor = colors.shift();
+                    colors.push(usedColor);
+                    var usedWord = words.shift();
+                    words.push(usedWord);
+                    x = 1;
+                    target.setAttribute("style", "color:" + colors[0]);
+                    letterCount += x;
+                    waiting = false;
+                  }, 1000);
+                } else if (letterCount === words[0].length + 1 && waiting === false) {
+                  waiting = true;
+                  window.setTimeout(function() {
+                    x = -1;
+                    letterCount += x;
+                    waiting = false;
+                  }, 1000);
+                } else if (waiting === false) {
+                  target.innerHTML = words[0].substring(0, letterCount);
+                  letterCount += x;
+                }
+              }, 120);
+              window.setInterval(function() {
+                if (visible === true) {
+                  con.className = "console-underscore hidden";
+                  visible = false;
+                } else {
+                  con.className = "console-underscore";
+
+                  visible = true;
+                }
+              }, 400);
+            }
+          </script>`;
           //add data to config.json
           const data = await getConfig();
           data[0].username = user.login;
